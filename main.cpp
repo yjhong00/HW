73c0dcb25e560456d4eea67921ea7d0e9c9b8cb5
yjhong00 <149555384+yjhong00@users.noreply.github.com>
2023-11-13 13:37:20 +0900

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
     store A;
-    A *= 2;  // Double the quantity of each fruit in store A
+    A *= 2;
 
-    cout << A.total() << endl;  // Display the new total after multiplication
+    cout << A.total() << endl;
 
     return 0;
 }
